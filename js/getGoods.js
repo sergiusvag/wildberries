02aeaf5ee62f0c9741c296583dba9d90d20103c3
sergiusvag <sergeyvagapov28@gmail.com>
2022-02-24 17:50:23 +0200

    init commit
    init commit

---
+++ js/getGoods.js
@@ -0,0 +1,51 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link')
+    const keyString = "DBKey-";
+    let keys = JSON.parse(localStorage.getItem('keys'));
+    let curKeyNum = localStorage.length + 1;
+    let curKeyString;
+
+    const getData = () => {
+        fetch('https://wildberries-db-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+            .then(res => res.json())
+            .then(data => {
+                setToLocalStorage(data);
+            })
+    }
+
+    // set the data to localstorage
+    const setToLocalStorage = (data) => {
+        curKeyString = keyString.concat(curKeyNum);
+        localStorage.setItem(curKeyString, JSON.stringify(data));
+        keys.push(curKeyString);
+        localStorage.setItem('keys', JSON.stringify(keys));
+        curKeyNum++;
+    }
+    
+    // remove all DBs stored till now
+    const removeAllFromLocalStorage = () => {
+        const totalDBKeys = localStorage.length;
+        for(let i = 0; i < totalDBKeys; i++ ) {
+            localStorage.removeItem(keys[i]);
+        }
+        localStorage.removeItem('keys');
+    }
+
+    // in case there are no keys
+    if (keys === null) {
+        keys = [];
+    }
+
+    // uncomment this function to clean all stored DBs
+    removeAllFromLocalStorage();
+
+    // 
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            getData();
+        })
+    })
+}
+
+getGoods()
