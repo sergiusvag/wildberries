f7eed7f7c6acf4224fd2e5b34aeba44399cd6811
Sergey Vagapov <sergeyvagapov28@gmail.com>
2022-02-25 20:52:33 +0200

    updated links

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,52 +1,73 @@
 const getGoods = () => {
-    const links = document.querySelectorAll('.navigation-link')
-    const keyString = "DBKey-";
-    let keys = JSON.parse(localStorage.getItem('keys'));
-    let curKeyNum = localStorage.length + 1;
-    let curKeyString;
+    // I added this part to simply add viewall link to the "links" without changing viewall classes
+    const viewAll = document.querySelector('.more');
+    let wasAdded = false;
+    if (viewAll && !viewAll.classList.contains('navigation-link')) {
+        viewAll.classList.add('navigation-link')
+        wasAdded = true;
+    }
 
-    const getData = () => {
+    const links = document.querySelectorAll('.navigation-link')
+
+    if (wasAdded) {
+        viewAll.classList.remove('navigation-link')
+    }
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+
+        goodsContainer.innerHTML = ""
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `
+            goodsContainer.append(goodBlock)
+        })
+    }
+
+
+    const getData = (value, category) => {
         fetch('https://wildberries-db-default-rtdb.europe-west1.firebasedatabase.app/db.json')
             .then(res => res.json())
             .then(data => {
-                setToLocalStorage(data);
+                const array = category ? data.filter(item => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname != "/goods.html") {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array);
+                }
             })
     }
 
-    // set the data to localstorage
-    const setToLocalStorage = (data) => {
-        curKeyString = keyString.concat(curKeyNum);
-        localStorage.setItem(curKeyString, JSON.stringify(data));
-        keys.push(curKeyString);
-        localStorage.setItem('keys', JSON.stringify(keys));
-        curKeyNum++;
-    }
-    
-    // remove all DBs stored till now
-    const removeAllFromLocalStorage = () => {
-        const totalDBKeys = localStorage.length;
-        for(let i = 0; i < totalDBKeys; i++ ) {
-            localStorage.removeItem(keys[i]);
-        }
-        localStorage.removeItem('keys');
-        keys = [];
-    }
-
-    // in case there are no keys
-    if (keys === null) {
-        keys = [];
-    }
-
-    // comment this function if you don't want to clean the db on refresh
-    removeAllFromLocalStorage();
-
-    // 
     links.forEach((link) => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            getData();
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getData(linkValue, category);
         })
     })
+
+    if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 }
 
 getGoods()
